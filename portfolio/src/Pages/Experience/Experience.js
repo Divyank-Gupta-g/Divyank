b66550810dbb255971fd73817673883899ccf7d3
Divyank-Gupta-g <divyankgupta001@gmail.com>
2025-02-02 19:16:12 +0530

    first commit

---
+++ portfolio/src/Pages/Experience/Experience.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Experience.css';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { MdWorkHistory } from "react-icons/md";
+
+const Experience = () => {
+  return (
+    <>
+        <div className='container-fluid experience-container' id='experience'>
+        <div className='container experience'>
+            <h2> Work Experience</h2>
+
+            <VerticalTimeline>
+            {/* <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                contentStyle={{ background: '#219ebc', color: '#fff' }}
+                contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                iconStyle={{ background: '#219ebc', color: '#ffb703' }}
+                icon={<MdWorkHistory />}
+            >
+                <h3 className="vertical-timeline-element-title">MERN Stack Developer Intern</h3>
+                <h4 className="vertical-timeline-element-subtitle">ASHPlan Pvt. Ltd.</h4>
+            </VerticalTimelineElement> */}
+
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                contentStyle={{ background: '#219ebc', color: '#fff' }}
+                contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                iconStyle={{ background: '#219ebc', color: '#ffb703' }}
+                icon={<MdWorkHistory />}
+            >
+                <h3 className="vertical-timeline-element-title">Software Developer Intern</h3>
+                <h4 className="vertical-timeline-element-subtitle">Dienken Technologies</h4>
+                <p>
+                Sep'24 - Present
+                </p>
+            </VerticalTimelineElement>
+            </VerticalTimeline>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Experience
